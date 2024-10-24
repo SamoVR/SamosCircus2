efc0681665b4897a2e9094bc8cb040eeb57f6cfa
SamoVR <samuelbrnak@gmail.com>
2024-10-24 17:28:35 +0200

    Revert "fixed problem with counting with commas"
    This reverts commit 2e2f4562dfe120999ee8c5dcc440d8ec98f35412.

--- WinFormsApp2/WinFormsApp2/Form1.cs
+++ WinFormsApp2/WinFormsApp2/Form1.cs
@@ -40,10 +40,9 @@ namespace WinFormsApp2
         {
             try
             {
-                string expression = richTextBox1.Text.Replace(',', '.');
 
                 DataTable dt = new DataTable();
-                var result = dt.Compute(expression, string.Empty);
+                var result = dt.Compute(richTextBox1.Text, string.Empty);
 
                 label1.Text = richTextBox1.Text + "=";
                 richTextBox1.Text = result.ToString();
@@ -53,7 +52,6 @@ namespace WinFormsApp2
                 MessageBox.Show("Invalid expression");
             }
         }
-
 
         private void AddNumbers(long number)
         {
