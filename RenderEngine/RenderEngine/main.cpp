585ec7c709c6bd8f1684d518c89f364255826cc1
Samo_VR <samuelbrnak@gmail.com>
2025-04-09 15:44:50 +0200

    added icon to the compiled exe file

--- RenderEngine/RenderEngine/main.cpp
+++ RenderEngine/RenderEngine/main.cpp
@@ -1,19 +1,27 @@
-#include "Engine.h"
 #include "Windows.h"
+#include "Engine.h"
+#include "resource.h"
 #include <iostream>
 
 int main() {
 //int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
-    {
-        try {
-            Engine engine;   // Create the Engine instance
-            engine.run();  // Run the engine loop
-        }
-        catch (const std::exception& e) {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return -1;
-        }
+    HINSTANCE hInstance = GetModuleHandle(nullptr);  // Get current instance handle !! THIS IS TO BE COMMENTED IN RELEASE VERSION !!
 
-        return 0;
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = DefWindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"RIFTWindowClass";
+    wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+    RegisterClass(&wc);
+
+    try {
+        Engine engine;
+        engine.run();
     }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return -1;
+    }
+
+    return 0;
 }
