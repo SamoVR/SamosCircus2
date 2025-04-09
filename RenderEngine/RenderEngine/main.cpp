7bba8a467ee11916d65fff803e3146798dd4b73f
Samo_VR <samuelbrnak@gmail.com>
2025-04-09 15:16:00 +0200

    Made project release ready

--- RenderEngine/RenderEngine/main.cpp
+++ RenderEngine/RenderEngine/main.cpp
@@ -1,16 +1,19 @@
 #include "Engine.h"
+#include "Windows.h"
 #include <iostream>
 
-int main()
-{
-    try {
-        Engine engine;   // Create the Engine instance
-        engine.run();  // Run the engine loop
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return -1;
-    }
+int main() {
+//int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+    {
+        try {
+            Engine engine;   // Create the Engine instance
+            engine.run();  // Run the engine loop
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            return -1;
+        }
 
-	return 0;
+        return 0;
+    }
 }
